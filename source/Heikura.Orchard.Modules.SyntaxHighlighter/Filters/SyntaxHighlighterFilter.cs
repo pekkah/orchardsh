7cb48c60767eb02f25b66af5a0f9394cf31472d0
Pekka Heikura <pekkah@users.noreply.github.com>
2010-10-17 21:09:18 +0300

    Initial commit. Does not compile.

---
+++ source/Heikura.Orchard.Modules.SyntaxHighlighter/Filters/SyntaxHighlighterFilter.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Mvc;
+using Orchard.Mvc.Filters;
+using Orchard.UI.Resources;
+
+namespace Heikura.Orchard.SyntaxHighlighter.Filters
+{
+    public class SyntaxHighlighterFilter : FilterProvider, IResultFilter {
+        private readonly IResourceManager _resourceManager;
+
+        public SyntaxHighlighterFilter(IResourceManager resourceManager) {
+            _resourceManager = resourceManager;
+        }
+
+        public void OnResultExecuting(ResultExecutingContext filterContext) {
+            _resourceManager.Require("script", ResourceManifest.CoreScript).AtHead();
+            _resourceManager.Require("script", ResourceManifest.BrushCSharp).AtHead();
+            _resourceManager.Require("script", ResourceManifest.BrushJScript).AtHead();
+
+            _resourceManager.Require("stylesheet", ResourceManifest.CoreStyle).AtHead();
+            _resourceManager.Require("stylesheet", ResourceManifest.ThemeDefault).AtHead();
+
+            _resourceManager.RegisterHeadScript("<script type=\"text/javascript\">SyntaxHighlighter.all()</script>");
+        }
+
+        public void OnResultExecuted(ResultExecutedContext filterContext) {
+        }
+    }
+}
---
+++ source/Heikura.Orchard.Modules.SyntaxHighlighter/ResourceManifest.cs
@@ -0,0 +1,37 @@
+using Orchard.UI.Resources;
+
+namespace Heikura.Orchard.SyntaxHighlighter
+{
+    public class ResourceManifest : IResourceManifestProvider {
+        public const string NamePrefix = "SH";
+
+        public const string CoreScript = NamePrefix + "script_core";
+        public const string CoreStyle = NamePrefix + "style_core";
+
+        // brushes
+        public const string BrushCSharp = NamePrefix + "brush_csharp";
+        public const string BrushJScript = NamePrefix + "brush_jscript";
+
+        // themes
+        public const string ThemeDefault = NamePrefix + "style_defaultTheme";
+
+        public void BuildManifests(ResourceManifestBuilder builder) {
+            var manifest = builder.Add();
+
+            // core scripts
+            manifest.DefineScript(CoreScript).SetUrl("shCore.js");
+
+            // define brush scripts 
+            // TODO (pekkah): Define a admin UI for configuring which brushes are included
+            manifest.DefineScript(BrushCSharp).SetUrl("shBrushCSharp.js");
+            manifest.DefineScript(BrushJScript).SetUrl("shBrushJScript.js");
+
+            // core styles
+            manifest.DefineStyle(CoreStyle).SetUrl("shCore.css");
+
+            // use default theme
+            // TODO (pekkah): Define a admin UI for configuring the used theme
+            manifest.DefineStyle(ThemeDefault).SetUrl("shThemeDefault.css");
+        }
+    }
+}
